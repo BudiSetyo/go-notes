e18a688b66972383dc6238cd83fc2ef85509506f
Budi Setyo <budisetyo176@gmail.com>
2023-09-04 10:55:42 +0700

    fix: task model user id remove required

--- models/task.go
+++ models/task.go
@@ -7,6 +7,6 @@ type Task struct {
 	Title       string `gorm:"not null" json:"title" validate:"required"`
 	Description string `json:"description" validate:"required"`
 	Status      string `json:"status" validate:"required"`
-	UserID      uint   `json:"user_id" validate:"required"`
+	UserID      uint   `json:"user_id"`
 	CategoryID  uint   `json:"category_id" validate:"required"`
 }
